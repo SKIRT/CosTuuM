3e2dbaf4becfd5a289ed7a67ec56b81aad7c5cba
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-10-02 14:04:01 +0100

    Fixed issue in TMatrixCalculator unit test: tolerance in Mishchenko code is actually smaller than nominal value. Made sure all input values for test are rounded. Adjusted test tolerances so that test now passes.

--- test/testTMatrixCalculator.cpp
+++ test/testTMatrixCalculator.cpp
@@ -8,6 +8,7 @@
 
 #include "Assert.hpp"
 #include "TMatrixCalculator.hpp"
+#include "UnitConverter.hpp"
 
 #include <fstream>
 #include <sstream>
@@ -32,7 +33,12 @@ int main(int argc, char **argv) {
   // skip the first comment line
   getline(ifile, line);
   // now read the other lines
+  uint_fast32_t counter = 0;
   while (getline(ifile, line)) {
+    ++counter;
+
+    ctm_warning("Line %" PRIuFAST32, counter);
+
     std::istringstream linestream(line);
     float_type axi, rat, lam, mrr, mri, eps, ddelt, alpha, beta, thet0, thet,
         phi0, phi, refqsca, refqext, refwalb, refZ[4][4];
@@ -45,31 +51,30 @@ int main(int argc, char **argv) {
         refZ[3][2] >> refZ[3][3];
 
     TMatrix *Tmat = TMatrixCalculator::calculate_TMatrix(
-        rat, eps, axi, lam, 200, ddelt, ndgs,
+        rat, eps, axi, lam, 200, ddelt * 0.1, ndgs,
         std::complex<float_type>(mrr, mri), 500);
 
     const float_type qext = Tmat->get_extinction_coefficient();
     const float_type qsca = Tmat->get_scattering_coefficient();
     const float_type walb = -qsca / qext;
 
-    const float_type deg_to_rad = M_PI / 180.;
-    alpha *= deg_to_rad;
-    beta *= deg_to_rad;
-    thet0 *= deg_to_rad;
-    thet *= deg_to_rad;
-    phi0 *= deg_to_rad;
-    phi *= deg_to_rad;
+    alpha = UnitConverter::to_SI<QUANTITY_ANGLE>(double(alpha), "degrees");
+    beta = UnitConverter::to_SI<QUANTITY_ANGLE>(double(beta), "degrees");
+    thet0 = UnitConverter::to_SI<QUANTITY_ANGLE>(double(thet0), "degrees");
+    thet = UnitConverter::to_SI<QUANTITY_ANGLE>(double(thet), "degrees");
+    phi0 = UnitConverter::to_SI<QUANTITY_ANGLE>(double(phi0), "degrees");
+    phi = UnitConverter::to_SI<QUANTITY_ANGLE>(double(phi), "degrees");
 
-    assert_values_equal_rel(double(qext), double(refqext), 1.e-3);
-    assert_values_equal_rel(double(qsca), double(refqsca), 1.e-3);
-    assert_values_equal_rel(double(walb), double(refwalb), 1.e-3);
+    assert_values_equal_rel(double(qext), double(refqext), 1.e-6);
+    assert_values_equal_rel(double(qsca), double(refqsca), 1.e-6);
+    assert_values_equal_rel(double(walb), double(refwalb), 1.e-6);
 
     Matrix<float_type> Z =
         Tmat->get_scattering_matrix(alpha, beta, thet0, phi0, thet, phi);
 
     for (uint_fast8_t i = 0; i < 4; ++i) {
       for (uint_fast8_t j = 0; j < 4; ++j) {
-        assert_values_equal_rel(double(Z(i, j)), double(refZ[i][j]), 0.1);
+        assert_values_equal_rel(double(Z(i, j)), double(refZ[i][j]), 2.e-2);
       }
     }
 
