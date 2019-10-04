c35d1f5ef1eb0d4a50b467ea08cea83afbf05126
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2019-10-04 09:56:49 +0100

    testQuickSched compiles and works, but crashes with a stack smash error on exit. Not sure why.

--- test/testQuickSched.cpp
+++ test/testQuickSched.cpp
@@ -6,20 +6,19 @@
  * @author Bert Vandenbroucke (bert.vandenbroucke@ugent.be)
  */
 
+extern "C" {
 #include "quicksched.h"
+}
 
 /**
  * @brief Matrix multiplication kernel.
  */
 
-void matmul(int m, int n, int k, double *a, int lda, double *b, int ldb,
-            double *c, int ldc) {
+void matmul(int m, int n, int k, const double *a, int lda, const double *b,
+            int ldb, double *c, int ldc) {
 
   int ii, jj, kk;
   double acc;
-
-  // message( "matmul: m=%i, n=%i, k=%i, lda=%i, ldb=%i, ldc=%i." ,
-  //     m , n , k , lda , ldb , ldc ); fflush(stdout);
 
   for (ii = 0; ii < m; ii++)
     for (jj = 0; jj < n; jj++) {
@@ -29,11 +28,27 @@ void matmul(int m, int n, int k, double *a, int lda, double *b, int ldb,
     }
 }
 
+struct task_data {
+  int d[2];
+  int m;
+  int n;
+  int k;
+  double *a;
+  double *b;
+  double *c;
+};
+
 /* Runner function to pass to the scheduler. */
 void runner(int type, void *data) {
 
   /* Decode the task data. */
-  int *d = (int *)data;
+  struct task_data *tdata = (struct task_data *)data;
+  const int k = tdata->k;
+  const int m = tdata->m;
+  const double *a = tdata->a;
+  const double *b = tdata->b;
+  double *c = tdata->b;
+  const int *d = tdata->d;
 
   /* Decode and execute the task. */
   switch (type) {
@@ -55,11 +70,16 @@ void runner(int type, void *data) {
  */
 int main(int argc, char **argv) {
 
-  int i, j, data[2];
+  const int nr_threads = 4;
+
+  int i, j;
   qsched_task_t tid;
   qsched_res_t rid;
   struct qsched s;
-  double *a, *b, *c, *res, irm = 1.0 / RAND_MAX;
+  double *a, *b, *c, irm = 1.0 / RAND_MAX;
+  int m = 4;
+  int n = 4;
+  int k = 4;
 
   /* Tell the user something about the test. */
   message("computing a tiled matrix multiplication of the form "
@@ -69,28 +89,36 @@ int main(int argc, char **argv) {
   bzero(&s, sizeof(struct qsched));
   qsched_init(&s, nr_threads, qsched_flag_none);
 
-  /* Allocate the matrices. */
-  if ((a = (double *)malloc(sizeof(double) * m * k * 32 * 32)) == NULL ||
-      (b = (double *)malloc(sizeof(double) * k * n * 32 * 32)) == NULL ||
-      (c = (double *)malloc(sizeof(double) * m * n * 32 * 32)) == NULL ||
-      (res = (double *)malloc(sizeof(double) * m * n * 32 * 32)) == NULL)
-    error("Failed to allocate matrices.");
+  a = new double[32 * 32 * m * k];
+  b = new double[32 * 32 * k * n];
+  c = new double[32 * 32 * m * n];
 
   /* Fill the matrices. */
-  for (i = 0; i < m * k * 32 * 32; i++)
+  for (i = 0; i < m * k * 32 * 32; i++) {
     a[i] = rand() * irm;
-  for (i = 0; i < k * n * 32 * 32; i++)
+  }
+  for (i = 0; i < k * n * 32 * 32; i++) {
     b[i] = rand() * irm;
-  bzero(c, sizeof(double) * m * n * 32 * 32);
-  bzero(res, sizeof(double) * m * n * 32 * 32);
+  }
+  for (i = 0; i < k * n * 32 * 32; i++) {
+    c[i] = 0.;
+  }
 
   /* Build a task for each tile of the matrix c. */
   for (i = 0; i < m; i++)
     for (j = 0; j < n; j++) {
-      data[0] = i;
-      data[1] = j;
+      struct task_data tdata;
+      tdata.d[0] = i;
+      tdata.d[1] = j;
+      tdata.m = m;
+      tdata.n = n;
+      tdata.k = k;
+      tdata.a = a;
+      tdata.b = b;
+      tdata.c = c;
       rid = qsched_addres(&s, qsched_owner_none, qsched_res_none);
-      tid = qsched_addtask(&s, 1, task_flag_none, data, 2 * sizeof(int), 1);
+      tid = qsched_addtask(&s, 1, task_flag_none, &tdata,
+                           sizeof(struct task_data), 1);
       qsched_addlock(&s, tid, rid);
     }
 
@@ -98,6 +126,10 @@ int main(int argc, char **argv) {
 
   /* Clean up. */
   qsched_free(&s);
+
+  delete[] a;
+  delete[] b;
+  delete[] c;
 
   return 0;
 }
