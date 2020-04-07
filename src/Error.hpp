fbdf377f1fd249d9fb7c9d56b3227d333bf2a558
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2020-04-07 11:06:55 +0200

    Added/updated license for all files in the src folder.

--- src/Error.hpp
+++ src/Error.hpp
@@ -1,8 +1,33 @@
+/*******************************************************************************
+ * This file is part of CosTuuM
+ * Copyright (C) 2016 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+ *               2019 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+ *
+ * CosTuuM is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * CosTuuM is distributed in the hope that it will be useful, but WITOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CosTuuM. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 /**
  * @file Error.hpp
  *
  * @brief Macros for custom, more verbose code abortion and messages.
  *
+ * This file was originally part of the photoionization code CMacIonize
+ * (https://github.com/bwvdnbro/CMacIonize). Macro names were changed to reflect
+ * the different name of this project. Additional assertion macros were added to
+ * check for NaNs.
+ *
+ * @author Bert Vandenbroucke (bv7@st-andrews.ac.uk)
  * @author Bert Vandenbroucke (bert.vandenbroucke@ugent.be)
  */
 #ifndef ERROR_HPP
