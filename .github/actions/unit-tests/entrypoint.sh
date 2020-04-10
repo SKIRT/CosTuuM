be3a7abe91b5366cc715dbcc4c798134554a6bf0
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2020-04-10 14:51:40 +0200

    Made sure error handling in unit test script works.

--- .github/actions/unit-tests/entrypoint.sh
+++ .github/actions/unit-tests/entrypoint.sh
@@ -17,38 +17,38 @@ exitcode=0
 
 # Configure the code using CMake
 cmake ..
-if $? -ne 0; then
+if [ $? -ne 0 ] ; then
   echo "Configuration failed!"
   exitcode=1
 fi
 
 # Now compile the code...
 make
-if $? -ne 0; then
+if [ $? -ne 0 ] ; then
   echo "Compilation failed!"
   exitcode=1
 fi
 # ...compile and run the unit tests...
 make check
-if $? -ne 0; then
+if [ $? -ne 0 ] ; then
   echo "Unit tests failed!"
   exitcode=1
 fi
 # ...install the Python library...
 make install
-if $? -ne 0; then
+if [ $? -ne 0 ] ; then
   echo "Installation failed!"
   exitcode=1
 fi
 # ...and check that we can actually load it from Python
 python3 -c "import CosTuuM"
-if $? -ne 0; then
+if [ $? -ne 0 ] ; then
   echo "Python module cannot be imported!"
   exitcode=1
 fi
 
 # Check if all tests passed.
-if $exitcode -ne 0; then
+if [ $exitcode -ne 0 ] ; then
   echo "One or more tests failed!"
   exit 1
 else
